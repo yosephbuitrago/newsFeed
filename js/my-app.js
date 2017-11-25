533a7fd5f731212feb5b5f39214db461dedc9445
Yoseph Buitrago <yosephbuitrago.01@gmail.com>
2017-11-25 00:23:17 +0000

    half way of loading news, pulling the news is working and render those

--- js/my-app.js
+++ js/my-app.js
@@ -1,10 +1,59 @@
+
+/**var news={
+  title : [],
+  description : [],
+  url: [],
+  urlToImage: [],
+  publishedAt: []
+};**/
+var newsAPI='https://newsapi.org/v2/top-headlines?sources=';
+var keyNewsAPI='&apiKey=955b003024764dda8ee94b4f3cb74e1e';
+var sources = ['bbc-news','bloomberg','cnn','engadget'];
+var listUrlSources=[];
+
+$.each(sources,function (key,val) {
+  listUrlSources.push(newsAPI+val+keyNewsAPI);
+});
+console.log(listUrlSources);
+
+
+var news = {
+  bbc:[]
+};
+//var news;
+$.getJSON( listUrlSources[0], function( data ) {
+  $.each( data.articles, function(key,val) {
+     /**news.title.push(val.title);
+     news.description.push(val.description);
+     news.url.push(val.url);
+     news.urlToImage.push(val.urlToImage);
+     news.publishedAt.push(val.publishedAt);**/
+     var article ={
+       title : val.title,
+       description : val.description,
+       url: val.url,
+       urlToImage:val.urlToImage,
+       publishedAt: val.publishedAt
+     }
+     news.bbc.push(article);
+     console.log(val.urlToImage);
+    });
+    console.log(news);
+});
+//console.log(news);
+
+console.log(news);
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7({
-
-  template7Pages: true
-
+  template7Pages: true,
+   template7Data: {
+     'url:about.html': {
+       articles : news.bbc
+     }
+   }
 
 });
+console.log(news);
 
 // We need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
@@ -14,81 +63,3 @@ var mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
 });
-
-// Now we need to run the code that will be executed only for About page.
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-
-myApp.onPageInit('about', function (page) {
-  // Do something here for "about" page
-  console.log("hi from 1");
-
-  getNews();
-
-});
-
-
-function getNews() {
-
-  var urlAPI = 'https://newsapi.org/v2/top-headlines?' +
-            'sources=bbc-news&' +
-            'apiKey=955b003024764dda8ee94b4f3cb74e1e';
-
-
-  var cards='';
-
-      $$.getJSON( "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=955b003024764dda8ee94b4f3cb74e1e", function( data ) {
-        $$.each( data.articles, function(val) {
-                //console.log(key+" value"+ val.title);
-                // cards =  +'<div class="card demo-card-header-pic">'+
-                //   '<div style="background-image:url(...)" valign="bottom" class="card-header color-white no-border">'+val.title+'</div>'+
-                //   '<div class="card-content">'+
-                //    '  <div class="card-content-inner">'+
-                //       '<p class="color-gray">'+val.publishedAt+'</p>'+
-                //       '<p>'+val.description+'</p>'+
-                //     '</div>'+
-                //   '</div>'+
-                //   '<div class="card-footer">'+
-                //     '<a href="#" class="link">Like</a>'+
-                //     '<a href="#" class="link">Read more</a>'+
-                //   '</div>'+
-                // '</div>';
-                // console.log(cards);
-                console.log(val);
-          });
-
-      });
-}
-
-/**$$.getJSON( "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=955b003024764dda8ee94b4f3cb74e1e", function( data ) {
-  $.each( data.articles, function(key,val) {
-          //console.log(key+" value"+ val.title);
-          // cards =  +'<div class="card demo-card-header-pic">'+
-          //   '<div style="background-image:url(...)" valign="bottom" class="card-header color-white no-border">'+val.title+'</div>'+
-          //   '<div class="card-content">'+
-          //    '  <div class="card-content-inner">'+
-          //       '<p class="color-gray">'+val.publishedAt+'</p>'+
-          //       '<p>'+val.description+'</p>'+
-          //     '</div>'+
-          //   '</div>'+
-          //   '<div class="card-footer">'+
-          //     '<a href="#" class="link">Like</a>'+
-          //     '<a href="#" class="link">Read more</a>'+
-          //   '</div>'+
-          // '</div>';
-          // console.log(cards);
-
-          $('#news').append(
-            `
-              <li>${val.author}</li>
-            `
-          );
-          console.log(val.author);
-    });
-    console.log(data);
-});
-
-$('#news').append(
-  `
-    <li>testttttt<li>
-  `
-);**/
