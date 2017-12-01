50d20cf9aa1c50b528cf0d25ebf20e146672bf23
Yoseph Buitrago <yosephbuitrago.01@gmail.com>
2017-12-01 09:57:35 +0000

    pulling the news almost done

--- js/my-app.js
+++ js/my-app.js
@@ -1,54 +1,60 @@
 
-/**var news={
-  title : [],
-  description : [],
-  url: [],
-  urlToImage: [],
-  publishedAt: []
-};**/
+
 var newsAPI='https://newsapi.org/v2/top-headlines?sources=';
 var keyNewsAPI='&apiKey=955b003024764dda8ee94b4f3cb74e1e';
-var sources = ['bbc-news','bloomberg','cnn','engadget'];
+var sources = ['cnn','bloomberg','engadget', 'bbc-news'];
 var listUrlSources=[];
 
 $.each(sources,function (key,val) {
   listUrlSources.push(newsAPI+val+keyNewsAPI);
+  getNews(newsAPI+val+keyNewsAPI,val);
 });
 console.log(listUrlSources);
 
 
 var news = {
-  bbc:[]
+  cnn:[],
+  bloomberg:[],
+  engadget:[],
+  'bbc-news':[]
 };
-//var news;
-$.getJSON( listUrlSources[0], function( data ) {
-  $.each( data.articles, function(key,val) {
-     /**news.title.push(val.title);
-     news.description.push(val.description);
-     news.url.push(val.url);
-     news.urlToImage.push(val.urlToImage);
-     news.publishedAt.push(val.publishedAt);**/
-     var article ={
-       title : val.title,
-       description : val.description,
-       url: val.url,
-       urlToImage:val.urlToImage,
-       publishedAt: val.publishedAt
-     }
-     news.bbc.push(article);
-     console.log(val.urlToImage);
-    });
-    console.log(news);
-});
-//console.log(news);
+
+function getNews(url, source) {
+  $.getJSON(url, function( data ) {
+     $.each( data.articles, function(key,val) {
+         var article ={
+           title : val.title,
+           description : val.description,
+           url: val.url,
+           urlToImage:val.urlToImage,
+           publishedAt: val.publishedAt
+         }
+         console.log("two ");
+         news[source].push(article);
+         //console.log(val.urlToImage);
+      });
+      //console.log(news);
+  });
+}
+
 
 console.log(news);
+
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7({
   template7Pages: true,
    template7Data: {
-     'url:about.html': {
-       articles : news.bbc
+     'url:cnn.html': {
+       listNews : news.cnn
+     },
+     'url:bbc.html': {
+       listNews : news['bbc-news']
+     },
+     'url:engadget.html': {
+       listNews : news.engadget
+     },
+     'url:bloomberg.html': {
+       listNews : news.bloomberg
      }
    }
 
@@ -62,4 +68,8 @@ var $$ = Dom7;
 var mainView = myApp.addView('.view-main', {
   // Because we want to use dynamic navbar, we need to enable it for this view:
   dynamicNavbar: true
+});
+var view2 = myApp.addView('#view-2', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
 });
